115ec5d76f24fc8fea6fe150cf19358b5cdd55d7
Prabhatgrg <prabhatgurung34@gmail.com>
2023-07-02 20:39:12 +0545

    Database Connection Done

--- index.php
+++ index.php
@@ -4,6 +4,17 @@
 define('FRONTEND', '/' . basename(__DIR__) . '/frontend');
 define('ABSPATH', '/' . basename(__DIR__));
 
+// Includes Global Functions
+require_once 'function.php';
+
+// Includes Backend Index
+require_once 'backend/index.php';
 
 //Open Database Connection
+$conn = open_con();
+session_start();
+
+require_once 'routes.php';
+
+close_con($conn);
 ?>
